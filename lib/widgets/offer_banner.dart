8fdce0d01bbbb402f9925398e63ba110ef57a083
Kwaku Owusu-Ansa <kwaku315@gmail.com>
2023-01-30 17:20:02 +0000

    added offer banner widget

---
+++ lib/widgets/offer_banner.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class OfferBanner extends StatelessWidget {
+  final String title;
+  final double pricePerMonth;
+  final double pricePerYear;
+  final void Function()? onTap;
+
+  const OfferBanner({
+    super.key,
+    required this.title,
+    required this.pricePerMonth,
+    required this.pricePerYear,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final titleWidget = Text(title);
+
+    final monthWidget = Text.rich(
+      TextSpan(
+        text: '$pricePerMonth ',
+        children: const [
+          TextSpan(text: 'per month'),
+        ],
+      ),
+    );
+    final yearWidget = Text.rich(
+      TextSpan(
+        text: '$pricePerYear ',
+        children: const [
+          TextSpan(text: 'per month'),
+        ],
+      ),
+    );
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      child: Row(
+        children: [
+          Image.asset('name'),
+          const SizedBox(
+            width: 16,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              titleWidget,
+              const SizedBox(
+                height: 16,
+              ),
+              monthWidget,
+              const SizedBox(
+                height: 16,
+              ),
+              yearWidget
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
